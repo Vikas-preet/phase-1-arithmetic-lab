bf56e58aa7be3f9cb4bb0da5df8dc80162784dc8
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-04-01 21:46:26 -0700

    lab done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const multiply = num1*num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 16;
+const num4 = 4;
+const mod = num3 / num4;
+
+const max = Math.max(5, 10, 18, 20, 2);
+
+
+
